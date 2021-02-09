71534afc5a5365413ebe4b09d51766a3e77c3732
SebastienOtten <sebastien.otten@student.ichec.be>
2021-02-09 14:24:24 +0100

    premier commit

---
+++ mesroutes.js
@@ -0,0 +1,24 @@
+let express = require ('express');
+let router = express.Router();
+//initialiser le tableau
+let todo = ["Faire les courses", "Nourir le chat"];
+
+//récupérer les inputs du formulaire todo et les ajouter à la liste
+
+router.get('/', (request, response) => {
+response.render('todo.ejs', {todolist : todo});
+ });
+
+router.get('/todo/new', (request, response) => {
+    item = request.query.todolabel
+    todo.push(item)
+    response.redirect('/')
+});
+
+//récupérer les inputs du formulaire sur le nom
+router.post('/', (request, response) =>{
+console.log(request.body);
+response.send("Hello " + request.body.myname);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,41 @@
+const { request, response } = require('express');
+let express = require('express');
+
+let app = express();
+
+app.use(express.urlencoded());
+
+
+
+//render todo page
+//app.get('/', (request, response) => {
+   // response.render('todo.ejs', {todolist : todo});
+//});
+
+//récupérer les inputs du formulaire todo et les ajouter à la liste
+//app.get('/todo/new', (request, response) => {
+   // item = request.query.todolabel
+    //todo.push(item)
+    //response.redirect('/')
+//});
+
+//récupérer les inputs du formulaire sur le nom
+//app.post('/', (request, response) =>{
+//console.log(request.body);
+//response.send("Hello " + request.body.myname);
+//});
+
+let routes = require('./mesroutes');
+app.use('/', routes);
+
+
+//appliquer le CSS
+app.use(express.static('public'));
+
+
+//fixer le port
+app.listen(3000, function() {
+    console.log('Server is running on port 3000')
+});
+
+
